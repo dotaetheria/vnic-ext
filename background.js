32b9d4cc6e770e06c35556df278e911e2c027c12
Tin Sever <web@sevetin.net>
2024-10-29 18:52:38 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,35 @@
+browser.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      const url = details.url;
+      const mnPatterns = ['mn://', 'mn%3A%2F%2F', 'mn%3a%2f%2f'];
+      
+      for (let pattern of mnPatterns) {
+        let index = url.indexOf(pattern);
+        if (index !== -1) {
+          let startIndex = index + pattern.length;
+          let domain = url.substring(startIndex).replace(/\/$/, '');
+          
+          domain = domain.split('?')[0].split('#')[0].split('&')[0];
+          
+          domain = decodeURIComponent(domain);
+          
+          return {
+            redirectUrl: `https://${domain}.vnic.tech`
+          };
+        }
+      }
+    },
+    {
+      urls: [
+        "<all_urls>",
+        "*://*/*mn://*",
+        "*://*/*mn%3A%2F%2F*",
+        "*://*/*mn%3a%2f%2f*",
+        "*://*/*vn://*",
+        "*://*/*vn%3A%2F%2F*",
+        "*://*/*vn%3a%2f%2f*"
+      ],
+      types: ["main_frame"]
+    },
+    ["blocking"]
+  );
